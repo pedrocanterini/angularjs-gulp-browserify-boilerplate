881437cdb2fb270d69b29eb11d8a8e8e6514755e
Pedro Canterini <pedro@piqora.com>
2015-05-14 23:04:55 -0700

    updated home controller

--- app/js/on_config.js
+++ app/js/on_config.js
@@ -10,7 +10,7 @@ function OnConfig($stateProvider, $locationProvider, $urlRouterProvider) {
   $stateProvider
   .state('Home', {
     url: '/',
-    controller: 'ExampleCtrl as home',
+    controller: 'HomeCtrl as home',
     templateUrl: 'home.html',
     title: 'Home'
   });
